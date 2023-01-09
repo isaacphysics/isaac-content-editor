56e65bd7370b0294319b3b6a23833d89de3ded87
Sean Morton <seanmorton@hey.com>
2023-01-09 15:22:01 -0800

    Add auth request headers

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -21,6 +21,10 @@ export async function doAuth(code: string) {
     }
 
     const result = await fetch(config.authUrl, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
       method: "POST",
       body: JSON.stringify(body)
     });
