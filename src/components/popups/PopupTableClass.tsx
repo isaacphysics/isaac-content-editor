b8bc3ef2b94a43d289710ec91345a55367033a7e
Sol Dubock <94075844+sjd210@users.noreply.github.com>
2025-06-26 12:49:36 +0100

    Add options to table popup

--- src/components/popups/PopupTableClass.tsx
+++ src/components/popups/PopupTableClass.tsx
@@ -1,13 +1,16 @@
 import React, {RefObject, useRef, useState} from "react";
 import {Popup, PopupCloseContext, PopupRef} from "./Popup";
-import {Button, Container, Input, Label} from "reactstrap";
+import {Button, Container, Input, InputGroup, Label} from "reactstrap";
 import {EditorView, ReactCodeMirrorRef} from "@uiw/react-codemirror";
 import styles from "../../styles/editor.module.css";
+import { isAda } from "../../services/site";
 
 export const PopupTableClass = ({wide, codemirror}: {wide: boolean, codemirror: RefObject<ReactCodeMirrorRef>}) => {
     const popupRef = useRef<PopupRef>(null);
 
     const [classes, setClasses] = useState<string>("");
+    const [expandable, setExpandable] = useState<boolean>(false);
+    const [scrollable, setScrollable] = useState<boolean>(false);
 
     const makeTableClass = (className: string) => (view: EditorView | undefined) => {
         if (!view) return false;
@@ -16,7 +19,7 @@ export const PopupTableClass = ({wide, codemirror}: {wide: boolean, codemirror: 
         const withAmendedClass = docText.includes('class=') ? 
             docText.replace(
                 /<table(.*class=")([A-Z,0-9 ]*)(".*)>/i,
-                `<table$1$2 ${className}$3>`
+                `<table$1${className}$3>`
             ) : 
             docText.replace(
                 /<table(.*)>/i,
@@ -36,15 +39,45 @@ export const PopupTableClass = ({wide, codemirror}: {wide: boolean, codemirror: 
     return <>
         <button className={styles.cmPanelButton} title={"Augment table"} onClick={(event) => {
             popupRef.current?.open(event);
+            const tableClasses = codemirror.current?.view?.state.doc.toString().match(/<table(.*class=")([A-Z,0-9 ]*)(".*)>/i)?.[2];
+            setClasses(tableClasses ?? "");
+            setExpandable(tableClasses?.includes("expandable") ?? false);
+            setScrollable(tableClasses?.includes("scrollable") ?? false);
         }}>{wide ? "Augment table" : "Table"}</button>
         <Popup popUpRef={popupRef}>
             <Container className={styles.cmPanelPopup}>
-                <Label for={"inline-part-classes"}>Classes:</Label>
-                <Input id={"inline-part-classes"} placeholder={"e.g. wf-8"} onChange={(e) => setClasses(e.target.value)}/>
+                {isAda && <InputGroup className={"pl-4"}>
+                    <Label for={"table-expandable"}>Expandable</Label>
+                    <Input type="checkbox" id="table-expandable" checked={expandable} onChange={e => { 
+                            setExpandable(e.target.checked);
+                            if (e.target.checked) {
+                                setClasses(prev => prev ? `${prev} expandable` : "expandable");
+                            } else {
+                                setClasses(prev => prev?.replace(/\bexpandable\b/g, "").trim());
+                            }
+                        }} 
+                    />
+                </InputGroup>}
+                <InputGroup className={"pl-4"}>
+                    <Label for={"table-scrollable"}>Top-scrollable</Label>
+                    <Input type="checkbox" id="table-scrollable" checked={scrollable} onChange={e => {
+                            setScrollable(e.target.checked);
+                            if (e.target.checked) {
+                                setClasses(prev => prev ? `${prev} scrollable` : "scrollable");
+                            } else {
+                                setClasses(prev => prev?.replace(/\bscrollable\b/g, "").trim());
+                            }
+                        }}/>
+                </InputGroup>
+                <Label for={"table-classes"}>Class:</Label>
+                <Input id={"table-classes"} placeholder={"e.g. text-centre"} value={classes} onChange={(e) => setClasses(e.target.value)}/>
                 <hr/>
                 <PopupCloseContext.Consumer>
-                    {close => <Button disabled={!classes} onClick={() => {
+                    {close => <Button disabled={!classes && !expandable && !scrollable} onClick={() => {
                         makeTableClass(classes)(codemirror.current?.view);
+                        setExpandable(false);
+                        setScrollable(false);
+                        setClasses("");
                         close?.();
                     }}>
                         Add class
@@ -52,5 +85,5 @@ export const PopupTableClass = ({wide, codemirror}: {wide: boolean, codemirror: 
                 </PopupCloseContext.Consumer>
             </Container>
         </Popup>
-    </>
+    </>;
 }
