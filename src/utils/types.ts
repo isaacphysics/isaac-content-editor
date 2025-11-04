1d0b8564e94e840637156eeff42c1715f11177c8
Jaycie Brown <jeab2@cam.ac.uk>
2025-11-04 14:49:45 +0000

    Fix KeysWithValsOfType type under optional arrays
    The type was removing the optional modifier once, but not in the `extends` clause meaning it was not giving all options

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,6 @@
 export type KeysWithValsOfType<T, V> =
     // {the subset of keys in T whose values extend V}[keyof T] - i.e. the keys of T whose values are of type V
-    {[K in keyof T]-?: T[K] extends V ? K : never}[keyof T];
+    {[K in keyof Required<T>]: Required<T>[K] extends V ? K : never}[keyof T];
 
 export function isDefined<T>(item: T | undefined): item is T {
     return item !== undefined;
