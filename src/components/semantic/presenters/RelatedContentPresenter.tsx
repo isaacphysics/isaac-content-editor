764c2229246413f04ca667c384f913d42f10c7c3
Jaycie Brown <jeab2@cam.ac.uk>
2026-08-11 09:56:50 +0100

    Use second endpoint rq to obtain fasttrack questions

--- src/components/semantic/presenters/RelatedContentPresenter.tsx
+++ src/components/semantic/presenters/RelatedContentPresenter.tsx
@@ -8,16 +8,26 @@ import { ArrayPropValueConstraintContext, ArrayPropPresenterInner } from "./Arra
 export function RelatedContentPresenter({doc, update}: PresenterProps) {
     const [searchString, setSearchString] = useState("");
 
-    const {data: relatedContent} = useSWR<{results: Content[]}>(
+    // TODO the site search endpoint currently doesn't include fasttrack questions, so we're using a separate endpoint call to ensure we can get those.
+    // TODO if/when site search allows fasttracks, merge these two calls together.
+
+    const {data: relatedNonFastTrack} = useSWR<{results: Content[]}>(
         searchString !== "" ? "search?query=" + encodeURIComponent(searchString) + "&types=isaacConceptPage,isaacQuestionPage" : null,
         stagingFetcher,
     );
 
-    return <ArrayPropValueConstraintContext.Provider value={{searchString, setSearchString, content: relatedContent?.results ?? [], mapContentToId: (c: Content) => c.id ?? ""}}>
+    const {data: relatedFastTrack} = useSWR<{results: Content[]}>(
+        searchString !== "" ? "pages/questions?searchString=" + encodeURIComponent(searchString) + "&fasttrack=true" : null,
+        stagingFetcher,
+    );
+
+    const relatedContent = [...(relatedNonFastTrack?.results ?? []), ...(relatedFastTrack?.results ?? [])];
+
+    return <ArrayPropValueConstraintContext.Provider value={{searchString, setSearchString, content: relatedContent ?? [], mapContentToId: (c: Content) => c.id ?? ""}}>
         <ArrayPropPresenterInner 
             doc={doc} update={update} prop="relatedContent" getChildId={(c: string) => c ?? ""} 
             calculateButtonProps={(c: Content) => ({
-                color: c.type === "isaacQuestionPage" ? "success" : "primary",
+                color: c.type === "isaacQuestionPage" ? "success" : c.type === "isaacFastTrackQuestionPage" ? "secondary" : "primary",
             })}
         />
     </ArrayPropValueConstraintContext.Provider>;
